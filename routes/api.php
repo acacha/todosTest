17df49fdebea8e2b8d5346f044700011a24942fe
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-10-21 14:29:38 +0200

    Sync

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::resource('tasks', 'TaskController');
+Route::resource('todos', 'TaskController');
